52c287e891b9467836ec8d4ef407b79f5148b2e8
Akita162 <cyan.gamers162@gmail.com>
2023-10-01 21:15:17 +0700

    edit home/main view

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('templates/topbar');
+        return view('home/main');
     }
 }
--- app/Views/home/main.php
+++ app/Views/home/main.php
@@ -1,4 +1,5 @@
+<?= $this->include('template/header') ?>
 <div class="container">
     <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos illo tenetur facilis, consequatur mollitia eligendi ex a dolorum officiis reiciendis quas nisi veritatis fugit excepturi unde nam aperiam enim qui perferendis modi voluptatem? Consequuntur a corrupti ad est tenetur fugit qui repudiandae, cupiditate mollitia eaque. Id, quis consequuntur deleniti possimus vitae magnam alias saepe est rerum a laudantium odio tempora deserunt earum fugit unde asperiores. Nesciunt veniam assumenda ducimus corporis tenetur perspiciatis repellat. Amet vero officia dolor? Alias iste veritatis sint atque, expedita perspiciatis cupiditate quam ipsa neque, quibusdam quod ab. Dicta iure similique voluptates incidunt reprehenderit veritatis reiciendis non?</h1>
 </div>
-    
+<?= $this->include('template/footer') ?>
