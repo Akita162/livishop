92512ae69021c6a33a99ed456a3f5bf525304374
Akita162 <cyan.gamers162@gmail.com>
2023-11-18 14:50:40 +0700

    change file format

--- app/Views/home/honkaisr.php
+++ app/Views/home/honkaisr.php
@@ -1,19 +1,17 @@
 <?= $this->include('templates/header') ?>
 <div class="container">
+    <div class="row">
 
-<div class="row">
-    <div class="col-12 text-center">
-        <img src="/assets/gambar/Heading.png" alt="Heading" class="img-heading">
+        <div class="col-12 text-center">
+            <img src="/assets/gambar/Heading.png" alt="Heading" class="img-heading">
+        </div>
+
+        <div class="col-4">
+            <a href="honkaisr.php">
+                <img src="/assets/gambar/Express Pass.png" alt="Express" class="img-express">
+            </a>
+        </div>
+
     </div>
-
-    <div class="col-4">
-    <a href="honkaisr.php">
-        <img src="/assets/gambar/Express Pass.png" alt="Express" class="img-express">
-        </a>        
-    </div>
-
-
-    </div>
-
 </div>
 <?= $this->include('templates/footer') ?>
