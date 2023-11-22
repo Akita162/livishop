8a286e53d5b7b45d55b40292fd6c398bf472fd47
YudMurZ <yuda01fadhilah@gmail.com>
2023-11-23 01:21:17 +0700

    nambah gambar metode pembayaran + direct

--- app/Views/home/honkaisr.php
+++ app/Views/home/honkaisr.php
@@ -6,9 +6,32 @@
             <img src="/assets/gambar/Heading.png" alt="Heading" class="img-heading">
         </div>
 
-        <div class="col-4">
+        <div class="col-4 offset-3 text-center">
             <a href="/game/hsr">
-                <img src="/assets/gambar/Express Pass.png" alt="Express" class="img-express">
+                <img src="/assets/gambar/Express Pass.png" alt="Express" class="img-express-big">
+            </a>
+        </div>
+
+        <div class="col-12 text-center mt-4">
+            <h2>Harga Express Pass Saat ini : Rp 57.000 (Update 20 Oktober)</h2>
+            <h2>Pilih Metode Pembayaran</h2>
+        </div>
+
+        <div class="col-4">
+            <a href="/bayar/dana">
+                <img src="/assets/gambar/logo dana.jpg" alt="Heading" class="img-small">
+            </a>
+        </div>
+
+        <div class="col-4">
+            <a href="/bayar/gopay">
+                <img src="/assets/gambar/logo gopay.jpg" alt="Heading" class="img-small">
+            </a>
+        </div>
+
+        <div class="col-4">
+            <a href="/bayar/bca">
+                <img src="/assets/gambar/Logo BCA_Biru.png" alt="Heading" class="img-small">
             </a>
         </div>
 
