32025f4e6806f3867abf30b0a364fbaf287fd3d1
Akita162 <cyan.gamers162@gmail.com>
2024-01-22 22:08:38 +0700

    remove unused element

--- app/Views/payment/dana.php
+++ app/Views/payment/dana.php
@@ -4,19 +4,12 @@
     <div class="container">
       <div class="col-6 ">
         <h5>Akun Dana : 123123123123</h5>
-        <h5>UID :
-          <?= $_POST['uid'] ?>
-        </h5>
-        <h5>Region :
-          <?= $_POST['region'] ?>
-        </h5>
-        <h5>Total yang harus di bayar : Rp 57.000</h5>
       </div>
 
       <div class="col-6">
         <h5>Bila sudah melakukan pembayaran, klik OK dan cek Inbox untuk mengecek
           informasi apakah sudah masuk apa belum </h5>
-        <button class="btn" onclick="location.href = '/home/';">OK</button>
+        <button class="btn" onclick="location.href = '/';">OK</button>
       </div>
     </div>
 
