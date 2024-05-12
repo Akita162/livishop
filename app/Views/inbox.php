b5dfa3c019b5aae550feceffc8dd7ffb440a9f52
YudMurZ <yuda01fadhilah@gmail.com>
2024-05-12 17:19:52 +0700

    form di inbox

--- app/Views/inbox.php
+++ app/Views/inbox.php
@@ -1,24 +1,45 @@
 <?= $this->include('templates/header') ?>
 
-<?php foreach ($pesanan as $p) { ?>
-    <center>
-        <div class="mb-3 col-12">
-            <h2>Pesanan Anda</h2>
-            <div class="justify-content-center">
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h6 class="card-subtitle mb-2 text-muted">UID:</h6>
-                        <span><?= $p['uid']; ?></span>
-                        <h6 class="card-subtitle mb-2 text-muted">Region:</h6>
-                        <span><?= $p['region']; ?></span>
-                        <p class="card-text">Pesanan:</p>
-                        <span><?= $p['item']; ?></span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </center>
-<?php } ?>
+
+<div class="container text-center">
+    <h2>Pesanan Anda</h2>
+
+    <form action="home\delete" method="post">
+
+        <ul>
+
+            <?php foreach ($pesanan as $p) { ?>
+                <center>
+                    <li>
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+
+                        <div class="mb-3 col-12">
+
+                            <div class="justify-content-center">
+                                <div class="card" style="width: 18rem;">
+                                    <div class="card-body">
+                                        <h6 class="card-subtitle mb-2 text-muted">UID:</h6>
+                                        <span>
+                                            <?= $p['uid']; ?>
+                                        </span>
+                                        <h6 class="card-subtitle mb-2 text-muted">Region:</h6>
+                                        <span>
+                                            <?= $p['region']; ?>
+                                        </span>
+                                        <p class="card-text">Pesanan:</p>
+                                        <span>
+                                            <?= $p['item']; ?>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </center>
+            <?php } ?>
+        </ul>
+    </form>
+</div>
 
 <center>
     <button type="submit" class="btn btn-primary w-25">
