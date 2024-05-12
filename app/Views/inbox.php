7c029ea267f387c22c4a0e41adca28e1614ef6fe
YudMurZ <yuda01fadhilah@gmail.com>
2024-05-12 14:21:45 +0700

    inbox design

--- app/Views/inbox.php
+++ app/Views/inbox.php
@@ -1,3 +1,18 @@
 <?= $this->include('templates/header') ?>
 
+<center>
+    <div class="mb-3 col-12">
+        <div class="justify-content-center">
+            <label for="inputUid" class="form-label">Pesanan Anda</label>
+
+
+        </div>
+</center>
+<center>
+    <button type="submit" class="btn btn-primary w-25">
+        Hapuskan pesanan
+    </button>
+</center>
+</div>
+
 <?= $this->include('templates/footer') ?>
