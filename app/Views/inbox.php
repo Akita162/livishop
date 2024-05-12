530920ce619aa550ce63e2977d0af122f8f62a9d
YudMurZ <yuda01fadhilah@gmail.com>
2024-05-12 14:28:17 +0700

    modif kotak buat template

--- app/Views/inbox.php
+++ app/Views/inbox.php
@@ -2,12 +2,29 @@
 
 <center>
     <div class="mb-3 col-12">
+        <h2>Pesanan Anda</h2>
         <div class="justify-content-center">
-            <label for="inputUid" class="form-label">Pesanan Anda</label>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama:
+                        <!-- Tempat Nama -->
+                    </h5>
+                    <h6 class="card-subtitle mb-2 text-muted">UID:
+                        <!-- Tempat UID -->
+                    </h6>
+                    <h6 class="card-subtitle mb-2 text-muted">Region:
+                        <!-- Tempat Region -->
+                    </h6>
+                    <p class="card-text">Pesanan:
+                        <!-- Tempat Pesanan -->
+                    </p>
+                </div>
 
 
-        </div>
+            </div>
 </center>
+
+
 <center>
     <button type="submit" class="btn btn-primary w-25">
         Hapuskan pesanan
